1a7bec0a8ff50b07ab1e22ff17496f14c3a20a6c
CentsOfFate <charlie.dalldorf@gmail.com>
2022-04-12 09:01:28 -0500

    Const Navbar

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme:
           ThemeData(primarySwatch: Colors.purple, brightness: Brightness.dark),
       debugShowCheckedModeBanner: false,
-      home: NavBar(),
+      home: const NavBar(),
     );
   }
 }
