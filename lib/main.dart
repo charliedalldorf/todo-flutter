a81d65cbdfd9ef5f2bb2bd02fc97eb3f1a009a2a
CentsOfFate <charlie.dalldorf@gmail.com>
2022-04-12 11:10:04 -0500

    Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      initialRoute: '/',
       theme:
           ThemeData(primarySwatch: Colors.purple, brightness: Brightness.dark),
       debugShowCheckedModeBanner: false,
